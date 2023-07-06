5a7480aa0543859f95b1d8e518da4c8bdd3ad96e
appsec <appsec>
2023-07-06 18:15:34 +0300

    First Commit

---
+++ content.js
@@ -0,0 +1,45 @@
+
+function inject() {
+    console.log("Passive Bounty Hunter is running 2...");
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach(node => {
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+                        const elements = Array.from(node.getElementsByTagName('*'));
+                        elements.forEach(element => {
+                            const attributes = Array.from(element.attributes).filter(attr => attr.name.startsWith('ng-'));
+
+                            let found = false;
+                            attributes.forEach(attr => {
+
+                                if (attr.value.includes('alert(1)')) {
+                                    found = true;
+                                }
+                                const initData = attr.value;
+
+                                console.log(
+                                    `Bounty: %c ${attr.name} ${initData}`, 'background: #222; color: #bada55',
+                                    element
+
+                                );
+                            });
+
+                            if (attributes.length > 0 && found) {
+                                element.style.backgroundColor = 'yellow';
+                                element.style.border = '1px solid black';
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    });
+
+    observer.observe(document, {childList: true, subtree: true});
+}
+
+
+console.log("Injecting...")
+inject();
+
---
+++ popup.js
@@ -0,0 +1,9 @@
+// popup.js
+document.getElementById('log-ng-init').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            files: ['content.js']
+        });
+    });
+});
